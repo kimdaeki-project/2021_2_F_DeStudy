7f66263410aa0650862958bea20429a5b346b92e
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-12-01 12:51:21 +0900

    파일업로더 준비

---
+++ src/main/java/com/example/demo/notice/NoticeFileVO.java
@@ -0,0 +1,34 @@
+package com.example.demo.notice;
+
+public class NoticeFileVO {
+
+	private Integer fileNum;
+	private Integer num;
+	private String fileName;
+	private String oriName;
+	
+	public Integer getFileNum() {
+		return fileNum;
+	}
+	public void setFileNum(Integer fileNum) {
+		this.fileNum = fileNum;
+	}
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getOriName() {
+		return oriName;
+	}
+	public void setOriName(String oriName) {
+		this.oriName = oriName;
+	}
+}
