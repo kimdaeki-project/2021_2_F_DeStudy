f7b7c65173955bf5c693060b68feaf168a683957
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-12-03 14:56:19 +0900

    선택결과에 따른 페이지 이동 성공

--- src/main/resources/static/js/choice.js
+++ src/main/resources/static/js/choice.js
@@ -2,8 +2,6 @@ const answer1 = ['컴퓨터공학 기초', 'HTML/CSS', 'Javascript', 'Java', 'SQ
 const answer2 = ['초급', '중급', '고급']
 const answer3 = ['기본서', '온라인 강의', '요약본']
 
-
-
 //버튼에 텍스트 , 클래스명 추가
 function addButton(answerText, idx) {
 	const button = document.createElement('button');
@@ -16,29 +14,39 @@ function addButton(answerText, idx) {
 		let link = window.location.href;
 		if(link == "http://localhost:8080/common/ch1"){
 			choice = $(this).attr('class');
-			let a = {"a" : choice};
+			let a = {"a1" : choice};
 			localStorage.setItem('a1', JSON.stringify(a));
 			location.href="ch2";
 		}else if(link == "http://localhost:8080/common/ch2"){
 			choice = $(this).attr('class');
-			let a = {"a" : choice};
+			let a = {"a2" : choice};
 			localStorage.setItem('a2', JSON.stringify(a));
 			location.href="ch3";
 		}else {
 			choice = $(this).attr('class');
-			let a = {"a" : choice};
+			let a = {"a3" : choice};
 			localStorage.setItem('a3', JSON.stringify(a));
 			result();
 		}
 	})
 }
-//
+
+//결과 출력 후 해당 페이지로 이동
 function result() {
-		const a1 = JSON.parse(localStorage.getItem('a1'));
-		const a2= JSON.parse(localStorage.getItem('a2'));
-		const a3= JSON.parse(localStorage.getItem('a3'));
-		const result = [a1, a2, a3];
-		console.log(result);
+		const r1 = JSON.parse(localStorage.getItem('a1'));
+		const r2= JSON.parse(localStorage.getItem('a2'));
+		const r3= JSON.parse(localStorage.getItem('a3'));
+		//console.log(r1.a1);
+		
+		if(r1.a1 == 0) {
+			//alert("collect!");
+			if(r2.a2 == 0) {
+				if(r3.a3 == 0) {
+					location.href="../result/basic_book";
+					return;
+				}
+			}			
+		}
 	}
 
 //버튼 추가
